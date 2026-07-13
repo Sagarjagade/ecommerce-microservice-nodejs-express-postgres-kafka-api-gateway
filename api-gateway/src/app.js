c8cdb69e2270b350cbd830ecbba8a103bbc4cc03
sagar jagade <sagarjagade9@gmail.com>
2026-07-13 17:08:16 +0530

    Add Helmet CORS and Rate Limiter

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,9 +1,32 @@
 import express from 'express'
 import logger from './config/logger.js';
 import morgan from 'morgan';
+import rateLimiter from './middleware/rateLimiter.middleware.js';
+import helmet from 'helmet';
+import cors from 'cors';
+
 const app = express();
 
 app.use(express.json());
+/**
+ * Security Headers
+ */
+app.use(helmet());
+app.use(rateLimiter());
+
+/**
+ * CORS
+ */
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:3000"
+        ],
+
+        credentials: true,
+    })
+);
 /** morgan logger */
 app.use(
     morgan(
